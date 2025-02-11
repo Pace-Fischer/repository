21fd69ec3bbe52f6800599ae5130800cdb1f790e
Pace-Fischer <pfischer002@stpaul.k12.mn.us>
2025-02-11 10:17:14 -0600

    z an

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -12,7 +12,7 @@ var config_data = `
     { "name": "Robot", "code": "r", "type": "robot", "choices": { "r1": "Red-1", "b1": "Blue-1<br>", "r2": "Red-2", "b2": "Blue-2<br>", "r3": "Red-3", "b3": "Blue-3" }, "required": "false" },
     { "name": "Team #", "code": "t", "type": "team", "min": 1, "max": 99999 },
     { "name": "Auto Start Position", "code": "as", "type": "clickable_image", "filename": "2025/half_field.png", "clickRestriction": "one", "dimensions": "6 6", "allowableResponses": "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36", "shape": "circle 5 black red true" },
-    { "name": "Preloaded?", "code": "pre", "type": "bool" }  <!-- Moved here from postmatch -->
+    { "name": "Preloaded?", "code": "pre", "type": "bool" }
   ],
   "auton": [
     { "name": "Leave Starting Line", "code": "al", "type": "bool" },
